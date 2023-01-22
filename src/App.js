466bf5819d823175f41cb024f754e8e6e2e88c2d
samueldobi <frankdobisamuel@gmail.com>
2023-01-22 13:28:28 +0100

    Added bootstrap and new files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"; 
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li className="btn btn-primary fw-bold">And</li>
+        <li>And</li>
+        <li>And</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
