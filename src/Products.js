6a2318803f02b8fa16b078e47166fa567646fe4e
samueldobi <69079873+samueldobi@users.noreply.github.com>
2023-03-01 16:03:21 +0100

    adding product box

--- src/Products.js
+++ src/Products.js
@@ -10,8 +10,8 @@ const Products = () => {
         <h1 className="fw-bold m-3">Products For You </h1>
         <Container>
             <Row>
-                <Col className="col-4">
-                <Card style={{ width: '18rem' }}>
+                <Col className="col-4 g-2">
+                <Card style={{ width: 'rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
@@ -23,8 +23,8 @@ const Products = () => {
       </Card.Body>
     </Card>
                 </Col>
-                <Col className="col-4">
-                <Card style={{ width: '18rem' }}>
+                <Col className="col-4 g-2">
+                <Card style={{ width: 'rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
@@ -36,8 +36,8 @@ const Products = () => {
       </Card.Body>
     </Card>
                 </Col>
-                <Col className="col-4">
-                <Card style={{ width: '18rem' }}>
+                <Col className="col-4 gx-1">
+                <Card style={{ width: 'rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
@@ -49,10 +49,35 @@ const Products = () => {
       </Card.Body>
     </Card>
                 </Col>
-                <Col className="col-4">
-                    
+                <Col className="col-4 g-2">
+                <Card style={{ width: 'rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+                </Col> 
+
+               
+                <Col className="col-4 g-2">
+                <Card style={{ width: 'rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
                 </Col>
-                <Col className="col-4"></Col>
+
                 <Col className="col-4"></Col>
                 <Col className="col-4"></Col>
                 <Col className="col-4"></Col>
