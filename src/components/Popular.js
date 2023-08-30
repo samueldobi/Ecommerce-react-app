43c8929267b81276537d2e2353c967cf5da3a361
samueldob <frankdobisamuel@gmail.com>
2023-08-30 15:27:14 +0100

    workinng on the popular section

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -13,19 +13,62 @@ const Popular = () => {
                       <div className="popular-img col-4">
                         <img src={gadgets.gadget1} alt="" className="img-fluid " />
                       </div>
-                      <div className="popular-text">
-                        Product name
+                      <div className="popular-text ">
+                       <p className="fs-1 fw-bold">Product </p>
+                       <p className="fs-5 product-details">Product Details </p>
                       </div>
                     </div>
                     <div className="col-12 col-lg-5 d-flex product-box p-3 my-4 ">
                     <div className="popular-img col-4">
-                        <img src={gadgets.gadget1} alt="" className="img-fluid " />
+                        <img src={gadgets.gadget2} alt="" className="img-fluid " />
                       </div>
-                      <div className="popular-text">
-                        Product name
+                      <div className="popular-text ">
+                       <p className="fs-1 fw-bold">Product </p>
+                       <p className="fs-5 product-details">Product Details </p>
                       </div>
                     </div>
                 </Row>
+                <Row className=" col-12 p-3 d-flex justify-content-between"> 
+                    <div className="col-12 col-lg-5 d-flex product-box p-3 my-4">
+                      <div className="popular-img col-4">
+                        <img src={gadgets.gadget1} alt="" className="img-fluid " />
+                      </div>
+                      <div className="popular-text ">
+                       <p className="fs-1 fw-bold">Product </p>
+                       <p className="fs-5 product-details">Product Details </p>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-5 d-flex product-box p-3 my-4 ">
+                    <div className="popular-img col-4">
+                        <img src={gadgets.gadget2} alt="" className="img-fluid " />
+                      </div>
+                      <div className="popular-text ">
+                       <p className="fs-1 fw-bold">Product </p>
+                       <p className="fs-5 product-details">Product Details </p>
+                      </div>
+                    </div>
+                </Row>
+                <Row className=" col-12 p-3 d-flex justify-content-between"> 
+                    <div className="col-12 col-lg-5 d-flex product-box p-3 my-4">
+                      <div className="popular-img col-4">
+                        <img src={gadgets.gadget1} alt="" className="img-fluid " />
+                      </div>
+                      <div className="popular-text ">
+                       <p className="fs-1 fw-bold">Product </p>
+                       <p className="fs-5 product-details">Product Details </p>
+                      </div>
+                    </div>
+                    <div className="col-12 col-lg-5 d-flex product-box p-3 my-4 ">
+                    <div className="popular-img col-4">
+                        <img src={gadgets.gadget2} alt="" className="img-fluid " />
+                      </div>
+                      <div className="popular-text ">
+                       <p className="fs-1 fw-bold">Product </p>
+                       <p className="fs-5 product-details">Product Details </p>
+                      </div>
+                    </div>
+                </Row>
+              
              
 
               
