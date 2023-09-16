348b80a0bfba5b3131e5827ee63cce91702b7e65
samueldob <frankdobisamuel@gmail.com>
2023-09-16 19:22:36 +0100

    fixed swiper

--- src/components/Discount.js
+++ src/components/Discount.js
@@ -1,21 +1,51 @@
+import  Container  from "react-bootstrap/Container"
+import Row  from "react-bootstrap/Row"
+import Col  from "react-bootstrap/Col"
+import  gadgets from '../images/index.js'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import React, { useRef, useState } from 'react';
+import Data from "./dummydata.json"
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-// import './styles.css';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
 
 // import required modules
-import { Navigation } from 'swiper/modules';
+// import { Pagination } from 'swiper/modules';
 
-export default function App() {
+const Discount = () => {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
+    <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        // modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide> 
+        <Col > 
+            <Card className="card-design m-3" >
+                <Card.Body>
+                    <Card.Title>Card Title 1</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+                </Card.Body>
+                <Card.Img variant="top" src={gadgets.gadget6} fluid/>
+                </Card>
+        </Col>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
@@ -29,3 +59,4 @@ export default function App() {
     
   );
 }
+export default Discount
