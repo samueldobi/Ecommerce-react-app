8cee2d660ca4f9025368211ed657af7394117c3b
samueldob <frankdobisamuel@gmail.com>
2023-10-12 02:18:17 +0100

    installed react router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import Popular from "./components/Popular";
 import Discount from "./components/Discount";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <Hero/>
@@ -14,7 +17,9 @@ function App() {
       <Products/>
       <Popular/>
       <Discount/>
+      <Footer/>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer p-3'>
+          <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <h5>Column 1</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+              <li><a href="#">Link 3</a></li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5>Column 2</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">Link 4</a></li>
+              <li><a href="#">Link 5</a></li>
+              <li><a href="#">Link 6</a></li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5>Column 3</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">Link 7</a></li>
+              <li><a href="#">Link 8</a></li>
+              <li><a href="#">Link 9</a></li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5>Column 4</h5>
+            <ul className="list-unstyled">
+              <li><a href="#">Link 10</a></li>
+              <li><a href="#">Link 11</a></li>
+              <li><a href="#">Link 12</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
